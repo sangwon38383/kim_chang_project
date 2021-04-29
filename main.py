9661b3e733fcf9c14d239260885edfdef563ab1a
inter <intern@intern.com>
2021-04-30 01:16:25 +0900

    aa

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ if __name__ == '__main__':
     parser.add_argument("--max_steps", default=-1, type=int, help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
     parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
 
-    parser.add_argument('--logging_steps', type=int, default=2000, help="Log every X updates steps.")
-    parser.add_argument('--save_steps', type=int, default=2000, help="Save checkpoint every X updates steps.")
+    parser.add_argument('--logging_steps', type=int, default=100, help="Log every X updates steps.")
+    parser.add_argument('--save_steps', type=int, default=100, help="Save checkpoint every X updates steps.")
 
     parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
     parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the test set.")
